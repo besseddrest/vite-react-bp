ee9feb34c3b95f120dcde54daafbdaa03802d35e
Harold Cabalic <haroldmbp2017@Harolds-MBP.attlocal.net>
2024-08-19 00:17:29 -0700

    Update to hello world

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,10 @@
 import './App.css'
 import './styles/custom.scss'
-import MyComponent from './components/MyComponent'
 
 function App() {
     return (
         <>
-            <h1>My App</h1>
-            <MyComponent />
+            <h1>Hello, World</h1>
         </>
     )
 }
