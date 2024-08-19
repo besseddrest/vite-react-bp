d2338f8294c6db52e31c9fe54d86bb578eba6204
Harold Cabalic <haroldmbp2017@Harolds-MBP.attlocal.net>
2024-08-18 23:43:24 -0700

    remove fake endpoing

--- server/index.js
+++ server/index.js
@@ -6,10 +6,6 @@ const PORT = 5174
 
 app.use(cors())
 
-app.get('/api/v1/foo', (req, res) => {
-    res.json({ message: 'Foo!' })
-})
-
 app.listen(PORT, () => {
     console.log(`Server is running on port ${PORT}`)
 })
